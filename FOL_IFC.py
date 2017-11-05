e9fad954e2764172b79502a4d0291c565ced9240
brianna-lehman <brilehman29@gmail.com>
2017-11-05 18:48:53 -0500

    replace and unify written

--- FOL_IFC.py
+++ FOL_IFC.py
@@ -27,11 +27,12 @@ def main():
 				print()
 
 def addFact(fact):
+	print("Adding %s to list of facts" %fact)
 	if fact == prove:
 		print("%s proved" %prove)
+		exit()
 
 	for rule_dict in rules:
-		print("Unifying %s with %s" %(fact,rule_dict))
 		# try to unify every rule in the rule dictionary with the given fact
 		for rule, isProved in rule_dict.items():
 			print("\t%s: %s" %(rule,isProved))
@@ -43,19 +44,19 @@ def addFact(fact):
 				for rule, isProved in rule_dict.items():
 					for var, constant in substitutions.items():
 						if var in getValues(rule):
-							# replace(American(x), {x:West}) -> American(West)
-							# replace(Nation(z), {x:West}) -> Nation(z)
-							newRule = replace(rule, substitutions)
+							newRule = replace(rule, var, constant)
 							rule_dict[newRule] = rule_dict.pop(rule)
+
 							# move the second to last dictionary element to the end of the dictionary
 							end_rule = rule_dict.keys()[-2]
 							end_rule_value = rule_dict.pop(end_rule)
 							rule_dict[end_rule] = end_rule_value
 
-					if !hasVariable(rule):
+					if not hasVariable(rule):
 						rule_dict[rule] = True
 				# stop trying to find a new unification in this dictionary
 				break
+			print("\t%s: %s" %(rule,isProved))
 
 		# if all the rules have been proved and the implied rule has no remaining variables
 		# remove this dictionary of rules from the list of all rules
@@ -71,11 +72,34 @@ def addFact(fact):
 def unify(rule, fact):
 	# if the variables in rule can be matched w/ the constants in fact
 	# return the dictionary of variable:constant substitutions
-	pass
+	substitutions = {}
+	rule_vals = getValues(rule)
+	fact_vals = getValues(fact)
+	if getPredicate(rule) != getPredicate(fact) or len(rule_vals) != len(fact_vals):
+		return None
+	if len(rule_vals) == len(fact_vals):
+		substitutions = dict(zip(rule_vals, fact_vals))
+
+	for a1, a2 in substitutions.items():
+		if isConstant(a1) and isConstant(a2) and a1 != a2:
+			return None
+		if isConstant(a1)
+			substitutions.pop(a1)
+
+	return substitutions
 
 def replace(rule, variable, constant):
-	# replace the variable in the rule with the constant
-	# return the modified rule
+	pattern = r"(?<=\(|,)("+variable+")"
+	return re.sub(pattern, constant, rule)
+
+def isVariable(val):
+	if len(val) == 1 and val.islower():
+		return True
+	else:
+		return False
+
+def isConstant(val):
+	return not isVariable(val)
 
 def hasVariable(atom):
 	# returns true if any value inside predicate() is a variable
@@ -85,7 +109,7 @@ def hasVariable(atom):
 		return False
 
 	for v in values:
-		if len(v) == 1 or v.islower():
+		if len(v) == 1 and v.islower():
 			return True
 
 	return False
